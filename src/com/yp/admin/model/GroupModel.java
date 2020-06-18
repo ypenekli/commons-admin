51dd164049321ab5fc4bd4849f239c52c27e32bd
Yuksel Penekli <ypenekli@gmail.com>
2020-06-18 20:13:48 +0300

    GroupModel

--- src/com/yp/admin/model/GroupModel.java
+++ src/com/yp/admin/model/GroupModel.java
@@ -1,5 +1,13 @@
 package com.yp.admin.model;
 
+import java.util.List;
+
+import com.yp.admin.data.Projects;
+
 public class GroupModel {
+	
+	public List<Projects> findGroupList(final Integer pUserId, final String pProjectId) {
+		return null;
+	}
 
 }
